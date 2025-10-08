b7eb638933e99a37c0d209d815e31bea81725666
tanuj-jain-cse <tanujjain1546@gmail.com>
2025-10-08 14:44:03 +0530

    Update models.py

--- weatherproject/apps/weather/models.py
+++ weatherproject/apps/weather/models.py
@@ -1,96 +1,34 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.conf import settings
 
 class Location(models.Model):
-    """Stores geographic locations without GIS dependencies"""
-    name = models.CharField(max_length=100, db_index=True)
-    latitude = models.DecimalField(
-        max_digits=9, 
-        decimal_places=6,
-        validators=[MinValueValidator(-90), MaxValueValidator(90)]
-    )
-    longitude = models.DecimalField(
-        max_digits=9,
-        decimal_places=6,
-        validators=[MinValueValidator(-180), MaxValueValidator(180)]
-    )
-    country = models.CharField(max_length=100, db_index=True)
-    elevation = models.IntegerField(null=True, blank=True)
-
-    class Meta:
-        indexes = [
-            models.Index(fields=['latitude', 'longitude']),
-        ]
-
+    name = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    country = models.CharField(max_length=100, blank=True)
+    
     def __str__(self):
-        return f"{self.name}, {self.country}"
+        return self.name
 
 class WeatherData(models.Model):
-    """Weather observations and forecasts"""
-    class WeatherType(models.IntegerChoices):
-        CLEAR = 0, 'Clear'
-        CLOUDY = 1, 'Cloudy'
-        RAIN = 2, 'Rain'
-        SNOW = 3, 'Snow'
-        THUNDERSTORM = 4, 'Thunderstorm'
-
-    location = models.ForeignKey(
-        Location,
-        on_delete=models.CASCADE,
-        related_name='weather_data'
-    )
-    timestamp = models.DateTimeField(db_index=True)
-    temperature = models.FloatField(
-        help_text="Temperature in °C",
-        validators=[MinValueValidator(-50), MaxValueValidator(60)]
-    )
-    humidity = models.FloatField(
-        help_text="Relative humidity in %",
-        validators=[MinValueValidator(0), MaxValueValidator(100)]
-    )
-    wind_speed = models.FloatField(help_text="Wind speed in km/h")
-    precipitation = models.FloatField(default=0, help_text="Precipitation in mm")
-    weather_type = models.IntegerField(choices=WeatherType.choices, default=WeatherType.CLEAR)
-    is_forecast = models.BooleanField(default=False)
-    forecast_day = models.PositiveSmallIntegerField(
-        null=True,
-        blank=True,
-        validators=[MaxValueValidator(14)]
-    )
-    flood_risk = models.FloatField(
-        default=0,
-        validators=[MinValueValidator(0), MaxValueValidator(100)]
-    )
-    storm_risk = models.FloatField(
-        default=0,
-        validators=[MinValueValidator(0), MaxValueValidator(100)]
-    )
-    wildfire_risk = models.FloatField(
-        default=0,
-        validators=[MinValueValidator(0), MaxValueValidator(100)]
-    )
-
-    class Meta:
-        indexes = [
-            models.Index(fields=['location', 'timestamp']),
-            models.Index(fields=['is_forecast', 'forecast_day']),
-        ]
-        ordering = ['-timestamp']
-
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField()
+    temperature = models.FloatField()
+    humidity = models.FloatField()
+    wind_speed = models.FloatField()
+    weather_type = models.IntegerField(choices=[
+        (0, 'Clear'), (1, 'Clouds'), (2, 'Rain'),
+        (3, 'Snow'), (4, 'Thunderstorm')
+    ])
+    
     def __str__(self):
-        return f"{self.location} | {self.timestamp.date()} | {self.get_weather_type_display()}"
+        return f"{self.location.name} - {self.timestamp}"
 
 class UserSearchHistory(models.Model):
-    """Tracks user weather searches"""
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='searches')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     search_time = models.DateTimeField(auto_now_add=True)
     via_api = models.BooleanField(default=False)
-
-    class Meta:
-        ordering = ['-search_time']
-        verbose_name_plural = 'User Search History'
-
+    
     def __str__(self):
-        return f"{self.user.username} searched {self.location.name} at {self.search_time}"
+        return f"{self.user} searched {self.location.name}"
